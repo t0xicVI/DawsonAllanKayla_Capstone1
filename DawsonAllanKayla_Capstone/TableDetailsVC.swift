f4cac726a1372dcbe8b09b8194bbb23f2f54857e
LisetteAntigua <antigua223@gmail.com>
2021-06-04 17:17:25 -0400

    I implemented table views with sections

--- DawsonAllanKayla_Capstone/TableDetailsVC.swift
+++ DawsonAllanKayla_Capstone/TableDetailsVC.swift
@@ -12,15 +12,15 @@ class TableDetailsVC: UIViewController {
     @IBOutlet weak var aDescription: UILabel!
     @IBOutlet weak var announcements: UILabel!
     
-    var annouce: Announcements?
-    
+    var annouce: ListOfAnnoucements!
+    var indexpath:Int = 0;
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        announcements.text = "\((annouce?.title)!) comes under the category \((annouce?.category)!)"
-        aDescription.text = "\((annouce?.description)!)"
+        announcements.text = "\((annouce.type))"
+        aDescription.text = "\((annouce.announcements[indexpath].description))"
     }
 
     override func didReceiveMemoryWarning() {
--- DawsonAllanKayla_Capstone/TableViewController.swift
+++ DawsonAllanKayla_Capstone/TableViewController.swift
@@ -7,33 +7,39 @@
 
 import UIKit
 
-class Announcements {
-    var title: String?
-    var category: String?
-    var description: String?
 
-    init(title:String, category:String, description: String) {
-        self.title = title
-        self.category = category
-        self.description = description
-    }
+struct ListOfAnnoucements{
+    
+    var type:String
+    var announcements:[Announcements]
+    
+}
+struct Announcements {
+    var title: String
+    var description: String
 }
 
 
 class TableViewController: UITableViewController {
 
     @IBOutlet var tableAnnouncements: UITableView!
-    var aArray = [Announcements]()
+    var aArray = [ListOfAnnoucements]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let macBook = Announcements(title: "MackBook pro", category: "Laptop", description: "This is a description")
-        aArray.append(macBook)
-        let macBookAir = Announcements(title: "MackBook air", category: "Laptop", description: "This is a description")
-        aArray.append(macBookAir)
+      
+let state = ListOfAnnoucements(type: "State", announcements: [Announcements(title: "State Announcement", description: "this is a state announcement"), Announcements(title: "State2", description:"Another state")])
+let county = ListOfAnnoucements(type: "County", announcements: [Announcements(title: "County Announcement", description: "this is a County announcement")])
+let city = ListOfAnnoucements(type: "City", announcements: [Announcements(title: "City Announcement", description: "this is a City announcement"), Announcements(title: "City 2", description: "This is second city announcement ")])
+        
+     
         
         
+        aArray.append(city)
+        aArray.append(county)
+        aArray.append(state)
+    
         tableAnnouncements.dataSource = self
         tableAnnouncements.delegate = self
 
@@ -42,30 +48,63 @@ class TableViewController: UITableViewController {
     
     //MARK:- UITableView methods
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return aArray.count
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return  aArray.count
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return aArray[section].announcements.count
+
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let announce = aArray[section]
+        
+        return announce.type
+    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCell(withIdentifier: "announcementstable")
         if cell == nil {
             cell = UITableViewCell(style: .subtitle, reuseIdentifier: "announcementstable")
         }
         
-        cell?.textLabel?.text = aArray[indexPath.row].title
-        cell?.detailTextLabel?.text = aArray[indexPath.row].category
+        let announce = aArray[indexPath.section]
+        let announce2 = announce.announcements[indexPath.row]
         
+        // Configure the cell...
+        cell?.textLabel?.text = announce2.title
+        cell?.detailTextLabel?.text = announce2.description
         return cell!
     }
-
-    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "showdetail", sender: self)
     }
+
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+           let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 40))
+          
+             
+           let lbl = UILabel(frame: CGRect(x: 15, y: 0, width: view.frame.width - 15, height: 40))
+           //lbl.font = UIFont.systemFont(ofSize: 20)
+        lbl.textColor = UIColor.red
+        lbl.font = UIFont.boldSystemFont(ofSize: 20)
+        lbl.text = aArray[section].type
+           view.addSubview(lbl)
+           return view
+         }
     
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+           return 40
+    }
+         
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+           return 40
+    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let destination = segue.destination as? TableDetailsVC {
-            destination.annouce = aArray[(tableAnnouncements.indexPathForSelectedRow?.row)!]
+            destination.annouce = aArray[(tableAnnouncements.indexPathForSelectedRow?.section)!]
+            destination.indexpath = tableAnnouncements.indexPathForSelectedRow?.row ?? 0
             tableAnnouncements.deselectRow(at: tableAnnouncements.indexPathForSelectedRow!, animated: true)
 
         }
